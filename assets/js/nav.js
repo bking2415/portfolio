669f8aaf9911b8aba484e7bd4c95b9a395fc7428
Brandon I. King <55815159+bking2415@users.noreply.github.com>
2020-05-28 00:26:44 -0400

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -37,7 +37,7 @@ function onScroll(event) {
 	$('#scrollHeader nav ul li a').removeClass("active");
       	currLink.addClass("active");
     } else {
-	$("#scrollHeader h1 a").css("font-weight", "900");
+	$("#scrollHeader h1 a").css("font-weight", "normal");
       	currLink.removeClass("active");
     }
   });
