9c9f28c71fc5f8a3eee255e0fe60128117fe7b9e
Brandon I. King <55815159+bking2415@users.noreply.github.com>
2020-05-28 00:03:08 -0400

    Update nav_header.js

--- assets/js/nav_header.js
+++ assets/js/nav_header.js
@@ -1,8 +1,9 @@
 // https://stackoverflow.com/questions/8755887/jquery-change-background-color-user-scroll
 
 $(document).ready(function(){       
-       var scroll_pos = $(this).scrollTop();
-       $(document).scroll(function() {  
+       var scroll_pos = 0;
+       $(document).scroll(function() {
+             var scroll_pos = $(this).scrollTop();
              if(scroll_pos > 48) {
                    $("#scrollHeader h1 a").css("font-weight", "900");
              } else {
