ba5080ccb39e028c5e12c5bb84e9a705c3a14916
Brandon I. King <55815159+bking2415@users.noreply.github.com>
2020-05-27 23:40:07 -0400

    Update nav_header.js

--- assets/js/nav_header.js
+++ assets/js/nav_header.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
             var scroll_pos = 0;
             $(document).scroll(function() { 
                 scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 3em) {
+                if(scroll_pos > 48px) {
                     $("#scrollHeader h1 a").css("font-weight", "900");
                     $("#scrollHeader h1 a").css("color", "red");
                 } else {
